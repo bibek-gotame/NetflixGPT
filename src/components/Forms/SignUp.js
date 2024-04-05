c6630ae550cee90281f99f14017c61bfc9590fb3
bibek-gotame <bibekgotame011@gmail.com>
2024-04-05 18:47:54 +0545

    ...

--- src/components/Forms/SignUp.js
+++ src/components/Forms/SignUp.js
@@ -14,13 +14,13 @@ const SignUp = () => {
           alt="background-imagee"
         />
       </div>
-      <form className="absolute w-[23rem] py-10 px-6 rounded-lg bg-black top-36 left-[40%] text-white bg-opacity-85">
+      <form className="absolute flex flex-col gap-3 w-[23rem] py-10 px-6 rounded-lg bg-black top-32 left-[40%] text-white bg-opacity-85">
         <h2 className="font-extrabold text-2xl my-2" >{isSignIn ? 'Sign In' : 'Sign Up'}</h2>
-{ !isSignIn  && <input type='text' placeholder='Enter your Full Name' className="w-full p-4 my-3 rounded-md bg-gray-800" />
+{ !isSignIn  && <input type='text' placeholder='Enter your Full Name' className="w-full p-4  rounded-md bg-gray-800" />
 }
-        <input type='email' placeholder='Enter your email' className="w-full p-4 my-3 rounded-md bg-gray-800" />
-        <input type='password' placeholder='Enter your password' className="w-full p-4 my-3  rounded-md bg-gray-800" />
-        <button className="w-full p-4  my-4 rounded-md bg-red-600 font-bold">{isSignIn ? 'Sign in' : 'Sign Up'}</button>
+        <input type='email' placeholder='Enter your email' className="w-full p-4  rounded-md bg-gray-800" />
+        <input type='password' placeholder='Enter your password' className="w-full p-4   rounded-md bg-gray-800" />
+        <button className="w-full p-4   rounded-md bg-red-600 font-bold">{isSignIn ? 'Sign in' : 'Sign Up'}</button>
         <p className='px-2 py-2  cursor-pointer' onClick={() => {
           setIsSignIn(!isSignIn)
         }}>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,8 @@
 
 function Header() {
-  // console.log('hii');
-
   return (
     <>
-      <div className="absolute px-8 bg-gradient-to-b from-black w-full z-10" >
+      <div className="absolute px-8  w-full z-10" >
         <img src=  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 alt='logo' className="w-44"/>
       </div>
