a43c2b5d8c0a8749c8b7dc542c49417d6857d881
bibek-gotame <bibekgotame011@gmail.com>
2024-04-08 15:22:07 +0545

    good practices :  unsubscribing the onAuthStateChange, constant file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import { auth } from "../../utils/firebase";
 import {  useNavigate } from 'react-router-dom'
 import { useEffect } from "react";
 import { addUser, removeUser } from "../../utils/store/userSlice";
+import { LOGO } from "../../utils/constant";
 
 function Header() {
   const user = useSelector((store) => store.user)
@@ -11,7 +12,7 @@ function Header() {
   const dispatch  = useDispatch()
 
   useEffect(() => {    //! im not getting this one
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName ,photoURL} = user;
         dispatch(addUser({ uid: uid, email: email, displayName: displayName ,photoURL : photoURL}))
@@ -21,6 +22,7 @@ function Header() {
         navigate('/')
       }
     });
+    return ()=> unsubscribe();
   }, [])
 
   const SignOut = () => {
@@ -34,7 +36,7 @@ function Header() {
       <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
 
         <div className="w-44 mx-auto md:mx-0" >
-          <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          <img src={LOGO}
             alt='logo' className="w-44" />
         </div>
 
