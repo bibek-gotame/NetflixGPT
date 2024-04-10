63e277540419c19d3c6ba4e031949df791bd23c8
bibek-gotame <bibekgotame011@gmail.com>
2024-04-10 12:56:15 +0545

    useNowPlaying hook created

---
+++ src/Hooks/useNowPlaying.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+import { API_OPTIONS, nowPLayingUrl } from "../utils/constant"
+import { addNowPlayingMovies } from "../utils/store/movieSlice"
+import { useDispatch } from 'react-redux'
+export const useNowPlaying = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        NowPlaying()
+    }, [])
+    
+    const NowPlaying = async () => {
+        const data = await fetch(nowPLayingUrl, API_OPTIONS)
+        const json = await data.json()
+        console.log(json);
+        dispatch(addNowPlayingMovies(json.results))
+    }
+
+}
