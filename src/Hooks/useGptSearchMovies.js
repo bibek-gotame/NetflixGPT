fc36e53d4f7285afb2a84a962681c0ac2c756319
bibek-gotame <bibekgotame011@gmail.com>
2024-04-14 17:02:14 +0545

    ....

--- src/Hooks/useGptSearchMovies.js
+++ src/Hooks/useGptSearchMovies.js
@@ -11,13 +11,13 @@ export const useGptSearchMovies = (Movie_name) => {
   const getGptSearchMovies = async () => {
     const data = await fetch(searchAPI + Movie_name, API_OPTIONS)
     const json = await data.json()
-console.log(json);
+    console.log(json);
     dispatch(addGptSearchMovies(json))
   }
 
   useEffect(() => {
-    !gptSearchMovies && 
-    getGptSearchMovies() // useeffect run on every render with which api being called in every rendering so to avoid, we checked is trailer already existed , no api will be called
+    !gptSearchMovies &&
+      getGptSearchMovies() // useeffect run on every render with which api being called in every rendering so to avoid, we checked is trailer already existed , no api will be called
   }, [])
 
 }
